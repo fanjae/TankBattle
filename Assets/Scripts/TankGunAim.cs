b559e57882222de5529171b3be1c570aae432d0f
FanJae <hjjang45@gmail.com>
2026-05-23 17:13:08 +0900

    feat : 탱크 포신 회전 구현
    탱크의 포신의 회전 구현, 최소 각도와 최대 각도를 Mathf.Clamp()를 사용하여 제한하고, 부모 기준으로 로컬 회전을하도록 처리했다.

---
+++ Assets/Scripts/TankGunAim.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    [SerializeField] 
+    private float pitchSpeed = 30f;
+
+    [SerializeField]
+    private float minPitch = -10;
+
+    [SerializeField]
+    private float maxPitch = 30f;
+
+    private float currentAngle;
+    
+    void Update()
+    {
+        float input = 0f;
+
+        if (Input.GetKey(KeyCode.UpArrow)) input -= 1f;
+        if (Input.GetKey(KeyCode.DownArrow)) input += 1f;
+
+        currentAngle += input * pitchSpeed * Time.deltaTime;
+
+        // 최소, 최대 각도 제한.
+        currentAngle = Mathf.Clamp(currentAngle, minPitch, maxPitch);
+
+        // 로컬 회전 값 명시
+        transform.localRotation = Quaternion.Euler(currentAngle, 0f, 0f);
+    }
+}
