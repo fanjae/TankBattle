96234ec6cc61c39ba196a2e33452e8d6f8badf11
FanJae <hjjang45@gmail.com>
2026-05-23 21:58:24 +0900

    Refactor : 초기 포신 회전값 보정 처리
    초기에 포신의 회전값이 제대로 처리되어 있지 않아서 이 부분을 처리하도록 수정

--- Assets/Scripts/TankGunAim.cs
+++ Assets/Scripts/TankGunAim.cs
@@ -6,13 +6,29 @@ public class TankGunAim : MonoBehaviour
     private float pitchSpeed = 30f;
 
     [SerializeField]
-    private float minPitch = -10;
+    private float minPitch = -10f;
 
     [SerializeField]
     private float maxPitch = 30f;
 
     private float currentAngle;
-    
+
+    private void Awake()
+    {
+        // 포신의 로컬 X 회전값
+        currentAngle = transform.localEulerAngles.x;  
+
+        // 180도 이상 초과 값 음수 각도로 보정.
+        // ~180~180도 형태로 처리하도록 
+        if (currentAngle > 180f) currentAngle -= 360f;
+
+        // 시작 각도에 따른 제한
+        currentAngle = Mathf.Clamp(currentAngle, minPitch, maxPitch);
+
+        // 로컬 회전 값 명시
+        transform.localRotation = Quaternion.Euler(currentAngle, 0f, 0f);
+    }
+
     void Update()
     {
         float input = 0f;
