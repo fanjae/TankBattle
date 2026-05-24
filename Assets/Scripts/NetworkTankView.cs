c9940a04a43e20ba88a704a8479dec21bd4baef4
FanJae <hjjang45@gmail.com>
2026-05-24 23:08:40 +0900

    feat : 탱크의 포신 회전값 변경 및 HP Bar 정보 갱신 구현
    탱크의 포신 회전 값이 거꾸로 되어있던 문제를 수정하고, HP Bar에 대한 정보를 출력하도록 처리하였습니다.

--- Assets/Scripts/NetworkTankView.cs
+++ Assets/Scripts/NetworkTankView.cs
@@ -5,6 +5,7 @@ public class NetworkTankView : MonoBehaviour
     [SerializeField] private int playerId;
     [SerializeField] private Transform turret;
     [SerializeField] private Transform gun;
+    [SerializeField] private TankHpBar hpBar;
 
     private Quaternion initialGunLocalRotation;
     public int PlayerId => playerId;
@@ -18,13 +19,17 @@ public class NetworkTankView : MonoBehaviour
     // 서버에서 받은 탱크 상태를 Unity 오브젝트에 반영
     public void ApplyState(TankState state)
     {
-        transform.position = new Vector3(state.X, transform.position.y, state.Z); // 탱크 위치 상태 반영
-        transform.rotation = Quaternion.Euler(0f, state.Angle, 0f); // 탱크 회전 상태 반영
+        transform.position = new Vector3(state.X, transform.position.y, state.Z);
 
-        if (turret != null) // 터렛의 회전 상태 반영
+        transform.rotation = Quaternion.Euler(0f, state.Angle, 0f);
+
+        if (turret != null)
             turret.localRotation = Quaternion.Euler(0f, state.TurretTurn, 0f);
 
-        if (gun != null) // 포신 각도 반영
-            gun.localRotation = initialGunLocalRotation * Quaternion.Euler(state.GunPitch, 0f, 0f);
+        if (gun != null)
+            gun.localRotation = initialGunLocalRotation * Quaternion.Euler(-state.GunPitch, 0f, 0f);
+
+        if (hpBar != null)
+            hpBar.SetHp(state.Hp);
     }
 }
