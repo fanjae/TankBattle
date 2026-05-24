31bc5e5c0004c3aea54496347b318796cc20240a
FanJae <hjjang45@gmail.com>
2026-05-24 16:12:33 +0900

    feat : 서버에서 받은 탱크 상태를 Unity 오브젝트에 반영하는 TankView 구현
    서버에서 받은 탱크 상태에 대한 정보를 Unity 오브젝트에 반영할 수 있도록 처리하하였습니다.

---
+++ Assets/Scripts/NetworkTankView.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class NetworkTankView : MonoBehaviour
+{
+    [SerializeField] private int playerId;
+    [SerializeField] private Transform turret;
+    [SerializeField] private Transform gun;
+
+    public int PlayerId => playerId;
+
+
+    // 서버에서 받은 탱크 상태를 Unity 오브젝트에 반영
+    public void ApplyState(TankState state)
+    {
+        transform.position = new Vector3(state.X, transform.position.y, state.Z); // 탱크 위치 상태 반영
+        transform.rotation = Quaternion.Euler(0f, state.Angle, 0f); // 탱크 회전 상태 반영
+
+        if (turret != null) // 터렛의 회전 상태 반영
+            turret.localRotation = Quaternion.Euler(0f, state.TurretTurn, 0f);
+
+        if (gun != null) // 포신 각도 반영
+            gun.localRotation = Quaternion.Euler(state.GunPitch, 0f, 0f);
+    }
+}
